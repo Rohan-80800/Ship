df93ca2334b43dcbedf4bdf283bdb9490885ecb1
Rohan Shete <rohandshete2003@gmail.com>
2025-09-16 08:20:28 +0530

    Updated Postman test file

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,6 @@ export const authenticateToken = (req, res, next) => {
     return res.status(401).json({ message: "Access token required" });
   }
 
-  console.log("TOKEN ", token);
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Invalid or expired token" });
